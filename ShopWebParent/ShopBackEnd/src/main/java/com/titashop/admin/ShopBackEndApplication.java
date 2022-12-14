c8bf085588cb09843d0de2de82261a1de293ba64
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-12-14 13:50:27 -0300

    Commit initial

---
+++ ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/ShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.titashop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBackEndApplication.class, args);
+	}
+
+}
---
+++ ShopWebParent/ShopFrontEnd/src/main/java/com/titashop/site/ShopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.titashop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopFrontEndApplication.class, args);
+	}
+
+}
