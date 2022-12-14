6c9163ac550a99eaa79b31031c9620d5845e14cf
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-12-14 19:31:31 -0300

    Adding logotypes and nav menu

--- ShopWebParent/ShopFrontEnd/src/main/java/com/titashop/site/MainController.java
+++ ShopWebParent/ShopFrontEnd/src/main/java/com/titashop/site/MainController.java
@@ -2,8 +2,10 @@ package com.titashop.site;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@RequestMapping("/titashop")
 public class MainController {
 	
 	@GetMapping("")
