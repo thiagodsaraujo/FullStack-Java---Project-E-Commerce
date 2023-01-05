2c430a9af07b7df7623fd0b580bce70b9c799e0d
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2023-01-05 12:51:58 -0300

    Test on Different Screen Sizes
    07:48 +

--- ShopCommon/src/main/java/com/titashop/common/entity/User.java
+++ ShopCommon/src/main/java/com/titashop/common/entity/User.java
@@ -160,4 +160,9 @@ public class User {
 
         return "/user-photos/" + this.id + "/" + this.photos;
     }
+
+    @Transient
+    public  String getFullName(){
+        return firstName + " " + lastName;
+    }
 }
