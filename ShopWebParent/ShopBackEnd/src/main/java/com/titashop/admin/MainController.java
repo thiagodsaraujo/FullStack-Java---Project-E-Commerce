216eaa189788f3ec52404ca6927c544fa94a49b7
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2023-01-05 21:05:10 -0300

    Seção 07: Code User Authentication - 49 - Login Page

--- ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/MainController.java
+++ ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/MainController.java
@@ -11,4 +11,9 @@ public class MainController {
 		return "index";
 	}
 
+	@GetMapping("/login")
+	public String viewLoginPage(){
+		return "login";
+	}
+
 }
--- ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/security/WebSecurityConfig.java
+++ ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.titashop.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,8 +22,20 @@ public class WebSecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("email")
+                .permitAll();
         return http.build();
     }
+
+
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().anyRequest("/images/**", "/js/**", "/webjars/**");
+//    }
+
 
 }
