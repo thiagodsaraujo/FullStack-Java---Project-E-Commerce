b24cf45381d027ce08f186208f9a6fd9c33ec9eb
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2023-02-07 14:31:07 -0300

    115. Code add Details(Specs) on Product Form

---
+++ ShopWebParent/ShopBackEnd/src/main/resources/static/js/product_form_details.js
@@ -0,0 +1,33 @@
+function addNextDetailSection(){
+
+    allDivDetails = $("[id^='divDetail']");
+    divDetailsCount = allDivDetails.length;
+
+    htmlDetailSection=`
+    <div class="form-inline" id="divDetail${divDetailsCount}">
+        <label class="m-3">Name:</label>
+        <input type="text" class="form-control w-25" name="detailNames" maxlength="255"/>
+        <label class="m-3">Value:</label>
+        <input type="text" class="form-control w-25" name="detailValues" maxlength="255"/>
+    </div>
+    `;
+
+    $("#divProductDetails").append(htmlDetailSection);
+
+    previousDivDetailSection = allDivDetails.last();
+    previousDivDetailID = previousDivDetailSection.attr("id");
+
+    htmlLinkRemove = `
+		<a class="btn fas fa-times-circle fa-2x icon-dark"
+		    href="javascript:removeDetailSectionById('${previousDivDetailID}')"
+			title="Remove this detail"></a>
+	`;
+
+    previousDivDetailSection.append(htmlLinkRemove);
+
+    $("input[name='detailNames']").last().focus();
+}
+
+function removeDetailSectionById(id){
+    $("#" + id).remove();
+}
