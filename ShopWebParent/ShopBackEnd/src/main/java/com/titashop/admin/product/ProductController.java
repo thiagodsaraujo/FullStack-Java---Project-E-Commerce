8a8f0eaba7ea3db3d3cc08c1c24bc5a7ba183b39
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2023-02-26 15:36:41 -0300

    116. Code Save Product's(specs)

--- ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/product/ProductController.java
+++ ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/product/ProductController.java
@@ -20,22 +20,17 @@ import java.util.List;
 
 @Controller
 public class ProductController {
-
-    @Autowired
-    private ProductService productService;
-
+    @Autowired private ProductService productService;
     @Autowired private BrandService brandService;
 
     @GetMapping("/products")
-    public String listAll(Model model){
-
-        var listProducts = productService.listAll();
+    public String listAll(Model model) {
+        List<Product> listProducts = productService.listAll();
 
         model.addAttribute("listProducts", listProducts);
 
         return "products/products";
     }
-
 
     @GetMapping("/products/new")
     public String newProduct(Model model) {
@@ -52,80 +47,88 @@ public class ProductController {
         return "products/product_form";
     }
 
-
     @PostMapping("/products/save")
-    public String saveProduct(Product product, RedirectAttributes redirectAttributes,
-                              @RequestParam("fileImage")MultipartFile mainImageMultipart,
-                              @RequestParam("extraImage")MultipartFile[] extraImageMultiparts) throws IOException {
-
+    public String saveProduct(Product product, RedirectAttributes ra,
+                              @RequestParam("fileImage") MultipartFile mainImageMultipart,
+                              @RequestParam("extraImage") MultipartFile[] extraImageMultiparts,
+                              @RequestParam(name = "detailNames", required = false) String[] detailNames,
+                              @RequestParam(name = "detailValues", required = false) String[] detailValues
+    )
+            throws IOException {
+        
         setMainImageName(mainImageMultipart, product);
         setExtraImageNames(extraImageMultiparts, product);
+        setProductDetails(detailNames, detailValues, product);
 
         Product savedProduct = productService.save(product);
+
         saveUploadedImages(mainImageMultipart, extraImageMultiparts, savedProduct);
 
-        redirectAttributes.addFlashAttribute("message",
-                "This product has been saved sucessfully!");
+        ra.addFlashAttribute("message", "The product has been saved successfully.");
 
         return "redirect:/products";
     }
 
+    private void setProductDetails(String[] detailNames, String[] detailValues, Product product) {
+        if (detailNames == null || detailNames.length == 0) return;
+
+        for (int count = 0; count < detailNames.length; count++) {
+            String name = detailNames[count];
+            String value = detailValues[count];
+
+            if (!name.isEmpty() && !value.isEmpty()) {
+                product.addDetail(name, value);
+            }
+        }
+    }
+
     private void saveUploadedImages(MultipartFile mainImageMultipart,
-                                    MultipartFile[] extraImageMultiparts,
-                                    Product savedProduct) throws IOException {
+                                    MultipartFile[] extraImageMultiparts, Product savedProduct) throws IOException {
         if (!mainImageMultipart.isEmpty()) {
             String fileName = StringUtils.cleanPath(mainImageMultipart.getOriginalFilename());
             String uploadDir = "../product-images/" + savedProduct.getId();
+
             FileUploadUtil.cleanDir(uploadDir);
             FileUploadUtil.saveFile(uploadDir, fileName, mainImageMultipart);
         }
 
-        if (extraImageMultiparts.length > 0){
-
+        if (extraImageMultiparts.length > 0) {
             String uploadDir = "../product-images/" + savedProduct.getId() + "/extras";
 
             for (MultipartFile multipartFile : extraImageMultiparts) {
-                if (!multipartFile.isEmpty()) continue;
+                if (multipartFile.isEmpty()) continue;
 
                 String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
                 FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-
             }
-            }
+        }
 
     }
 
     private void setExtraImageNames(MultipartFile[] extraImageMultiparts, Product product) {
-
-        if (extraImageMultiparts.length > 0){
-
-            for (MultipartFile multipartFile : extraImageMultiparts){
-
-                if (!multipartFile.isEmpty()){
+        if (extraImageMultiparts.length > 0) {
+            for (MultipartFile multipartFile : extraImageMultiparts) {
+                if (!multipartFile.isEmpty()) {
                     String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
                     product.addExtraImage(fileName);
                 }
             }
-
         }
     }
 
-    private void setMainImageName(MultipartFile mainImageMultipart, Product product){
-
+    private void setMainImageName(MultipartFile mainImageMultipart, Product product) {
         if (!mainImageMultipart.isEmpty()) {
             String fileName = StringUtils.cleanPath(mainImageMultipart.getOriginalFilename());
             product.setMainImage(fileName);
         }
-
     }
 
     @GetMapping("/products/{id}/enabled/{status}")
-    public String updateProductEnabledStatus(@PathVariable("id") Integer id,
-                                             @PathVariable("status") boolean enabled,
-                                             RedirectAttributes redirectAttributes){
+    public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
+                                              @PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
         productService.updateProductEnabledStatus(id, enabled);
         String status = enabled ? "enabled" : "disabled";
-        String message = "The Product ID: " + id + " has been " + status;
+        String message = "The Product ID " + id + " has been " + status;
         redirectAttributes.addFlashAttribute("message", message);
 
         return "redirect:/products";
@@ -134,39 +137,23 @@ public class ProductController {
     @GetMapping("/products/delete/{id}")
     public String deleteProduct(@PathVariable(name = "id") Integer id,
                                 Model model,
-                                RedirectAttributes redirectAttributes){
-        try{
+                                RedirectAttributes redirectAttributes) {
+        try {
             productService.deleteProduct(id);
-            String productExtraImagesDirectory = "../product-images/" + id + "/extras";
-            String productImagesDirectory = "../product-images/" + id;
-            FileUploadUtil.removeDir(productExtraImagesDirectory);
-            FileUploadUtil.removeDir(productImagesDirectory);
+            String productExtraImagesDir = "../product-images/" + id + "/extras";
+            String productImagesDir = "../product-images/" + id;
 
-            redirectAttributes.addFlashAttribute("message", "The prodcut ID: " + id + " has been deleted sucessfully!");
-        } catch (ProductNotFoundException ex){
+            FileUploadUtil.removeDir(productExtraImagesDir);
+            FileUploadUtil.removeDir(productImagesDir);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "The product ID " + id + " has been deleted successfully");
+        } catch (ProductNotFoundException ex) {
             redirectAttributes.addFlashAttribute("message", ex.getMessage());
         }
+
         return "redirect:/products";
     }
-
-//    @GetMapping("/products/edit/{id}")
-//    public String editBrand(@PathVariable(name = "id") Integer id,
-//                            Model model,
-//                            RedirectAttributes redirectAttributes){
-//        try {
-//            var product = productService.get(id);
-//            List<Category> listCategories = categoryService.listCategoriesUsedInForm();
-//
-//            model.addAttribute("brand", brand);
-//            model.addAttribute("listCategories", listCategories);
-//            model.addAttribute("pageTitle", "Edit Brand (ID: " + id + ")");
-//
-//            return "brands/brand_form";
-//        } catch (ProductNotFoundException ex) {
-//            redirectAttributes.addFlashAttribute("message", ex.getMessage());
-//            return "redirect:/brands";
-//        }
-//    }
 }
 
 
--- ShopWebParent/ShopBackEnd/src/main/resources/static/js/product_form_details.js
+++ ShopWebParent/ShopBackEnd/src/main/resources/static/js/product_form_details.js
@@ -1,16 +1,24 @@
-function addNextDetailSection(){
+$(document).ready(function() {
+    $("a[name='linkRemoveDetail']").each(function(index) {
+        $(this).click(function() {
+            removeDetailSectionByIndex(index);
+        });
+    });
 
+});
+
+function addNextDetailSection() {
     allDivDetails = $("[id^='divDetail']");
     divDetailsCount = allDivDetails.length;
 
-    htmlDetailSection=`
-    <div class="form-inline" id="divDetail${divDetailsCount}">
-        <label class="m-3">Name:</label>
-        <input type="text" class="form-control w-25" name="detailNames" maxlength="255"/>
-        <label class="m-3">Value:</label>
-        <input type="text" class="form-control w-25" name="detailValues" maxlength="255"/>
-    </div>
-    `;
+    htmlDetailSection = `
+		<div class="form-inline" id="divDetail${divDetailsCount}">
+			<label class="m-3">Name:</label>
+			<input type="text" class="form-control w-25" name="detailNames" maxlength="255" />
+			<label class="m-3">Value:</label>
+			<input type="text" class="form-control w-25" name="detailValues" maxlength="255" />
+		</div>	
+	`;
 
     $("#divProductDetails").append(htmlDetailSection);
 
@@ -19,7 +27,7 @@ function addNextDetailSection(){
 
     htmlLinkRemove = `
 		<a class="btn fas fa-times-circle fa-2x icon-dark"
-		    href="javascript:removeDetailSectionById('${previousDivDetailID}')"
+			href="javascript:removeDetailSectionById('${previousDivDetailID}')"
 			title="Remove this detail"></a>
 	`;
 
@@ -28,6 +36,10 @@ function addNextDetailSection(){
     $("input[name='detailNames']").last().focus();
 }
 
-function removeDetailSectionById(id){
+function removeDetailSectionById(id) {
     $("#" + id).remove();
+}
+
+function removeDetailSectionByIndex(index) {
+    $("#divDetail" + index).remove();
 }
