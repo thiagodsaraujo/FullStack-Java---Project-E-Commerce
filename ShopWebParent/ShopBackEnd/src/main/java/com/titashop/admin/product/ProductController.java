36dbf4774c1a97a3348530b1f6783bdc416b7334
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2023-04-06 14:23:26 -0300

    119. Code view product details in Modal DIalog

--- ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/product/ProductController.java
+++ ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/product/ProductController.java
@@ -240,6 +240,22 @@ public class ProductController {
             return "redirect:/products";
         }
     }
+
+    @GetMapping("/products/detail/{id}")
+    public String viewProductDetails(@PathVariable("id") Integer id, Model model,
+                              RedirectAttributes ra) {
+        try {
+            Product product = productService.get(id);
+            model.addAttribute("product", product);
+
+            return "products/product_detail_modal";
+
+        } catch (ProductNotFoundException e) {
+            ra.addFlashAttribute("message", e.getMessage());
+
+            return "redirect:/products";
+        }
+    }
 }
 
 
