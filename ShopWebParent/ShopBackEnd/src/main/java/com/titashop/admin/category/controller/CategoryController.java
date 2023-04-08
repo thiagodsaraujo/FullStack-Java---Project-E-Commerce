31dcbc6dfa72fdff1412fa47c0a37d80a0565319
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2023-04-08 09:02:29 -0300

    122. Ajuste Modal

--- ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/category/controller/CategoryController.java
+++ ShopWebParent/ShopBackEnd/src/main/java/com/titashop/admin/category/controller/CategoryController.java
@@ -23,16 +23,12 @@ import java.util.List;
 
 @Controller
 public class CategoryController {
-
     @Autowired
     private CategoryService service;
 
-
     @GetMapping("/categories")
-    public String listFirstPage(@Param("sortDir") String sortDir,
-                          Model model){
+    public String listFirstPage(@Param("sortDir") String sortDir, Model model) {
         return listByPage(1, sortDir, null, model);
-
     }
 
     @GetMapping("/categories/page/{pageNum}")
@@ -71,21 +67,21 @@ public class CategoryController {
     }
 
     @GetMapping("/categories/new")
-    public String newCategory(Model model){
+    public String newCategory(Model model) {
         List<Category> listCategories = service.listCategoriesUsedInForm();
 
         model.addAttribute("category", new Category());
         model.addAttribute("listCategories", listCategories);
-        model.addAttribute("pageTitle", "Create new Category");
+        model.addAttribute("pageTitle", "Create New Category");
+
         return "categories/category_form";
     }
 
     @PostMapping("/categories/save")
     public String saveCategory(Category category,
-                               @RequestParam("fileImage")MultipartFile multipartFile,
-                               RedirectAttributes redirectAttributes) throws IOException {
-
-        if (!multipartFile.isEmpty()){
+                               @RequestParam("fileImage") MultipartFile multipartFile,
+                               RedirectAttributes ra) throws IOException {
+        if (!multipartFile.isEmpty()) {
             String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
             category.setImage(fileName);
 
@@ -98,14 +94,13 @@ public class CategoryController {
             service.save(category);
         }
 
-        redirectAttributes.addFlashAttribute("message", "The category has been save successfully.");
-
+        ra.addFlashAttribute("message", "The category has been saved successfully.");
         return "redirect:/categories";
     }
 
     @GetMapping("/categories/edit/{id}")
     public String editCategory(@PathVariable(name = "id") Integer id, Model model,
-                               RedirectAttributes redirectAttributes){
+                               RedirectAttributes ra) {
         try {
             Category category = service.get(id);
             List<Category> listCategories = service.listCategoriesUsedInForm();
@@ -115,21 +110,18 @@ public class CategoryController {
             model.addAttribute("pageTitle", "Edit Category (ID: " + id + ")");
 
             return "categories/category_form";
-        } catch (CategoryNotFoundException exception){
-            redirectAttributes.addFlashAttribute("message", exception.getMessage());
+        } catch (CategoryNotFoundException ex) {
+            ra.addFlashAttribute("message", ex.getMessage());
             return "redirect:/categories";
         }
     }
 
     @GetMapping("/categories/{id}/enabled/{status}")
     public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
-                                              @PathVariable("status") boolean enabled,
-                                              RedirectAttributes redirectAttributes){
-
+                                              @PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
         service.updateCategoryEnabledStatus(id, enabled);
         String status = enabled ? "enabled" : "disabled";
-        String message = "The Category ID: " + id + " has been " + status;
-
+        String message = "The category ID " + id + " has been " + status;
         redirectAttributes.addFlashAttribute("message", message);
 
         return "redirect:/categories";
@@ -138,28 +130,25 @@ public class CategoryController {
     @GetMapping("/categories/delete/{id}")
     public String deleteCategory(@PathVariable(name = "id") Integer id,
                                  Model model,
-                                 RedirectAttributes redirectAttributes){
+                                 RedirectAttributes redirectAttributes) {
         try {
             service.delete(id);
             String categoryDir = "../category-images/" + id;
             FileUploadUtil.removeDir(categoryDir);
 
             redirectAttributes.addFlashAttribute("message",
-                    "The category ID " + id + " has been deleted successfully!");
-
-        } catch (CategoryNotFoundException exception){
-            redirectAttributes.addFlashAttribute("message", exception.getMessage());
+                    "The category ID " + id + " has been deleted successfully");
+        } catch (CategoryNotFoundException ex) {
+            redirectAttributes.addFlashAttribute("message", ex.getMessage());
         }
+
         return "redirect:/categories";
     }
 
-
     @GetMapping("/categories/export/csv")
-    public void exportToCSV(HttpServletResponse response) throws IOException    {
+    public void exportToCSV(HttpServletResponse response) throws IOException {
         List<Category> listCategories = service.listCategoriesUsedInForm();
         CategoryCsvExporter exporter = new CategoryCsvExporter();
         exporter.export(listCategories, response);
     }
-
-
 }
