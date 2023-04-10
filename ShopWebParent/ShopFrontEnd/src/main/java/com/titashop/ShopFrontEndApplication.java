aa9f697fc0715cdaa02626d900cf9fd5b61acda1
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2023-04-10 19:04:47 -0300

    129. Code Home Page for Shopping App

--- ShopWebParent/ShopFrontEnd/src/main/java/com/titashop/ShopFrontEndApplication.java
+++ ShopWebParent/ShopFrontEnd/src/main/java/com/titashop/ShopFrontEndApplication.java
@@ -1,4 +1,4 @@
-package com.titashop.site;
+package com.titashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ ShopWebParent/ShopFrontEnd/src/main/java/com/titashop/security/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.titashop.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+}
