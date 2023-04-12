c43ce31040a27b72df7ecbcea1f6406339c2ce93
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2023-04-12 12:50:26 -0300

    131. Display Product Price

--- ShopCommon/src/main/java/com/titashop/common/entity/Product.java
+++ ShopCommon/src/main/java/com/titashop/common/entity/Product.java
@@ -277,4 +277,12 @@ public class Product {
         return name;
     }
 
+    @Transient
+    public float getDiscountPrice(){
+        if (discountPercent > 0){
+            return price * ((100 - discountPercent) / 100);
+        }
+        return this.price;
+    }
+
 }
